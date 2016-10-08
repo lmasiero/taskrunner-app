e93e2846ce369be814d89409129a964a77ab2a51
Nick Ang <nickangtc@gmail.com>
2016-10-08 15:57:43 +0800

    Create 7 actions for category controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,22 +1,55 @@
 class CategoriesController < ApplicationController
   def index
-  end
-
-  def create
-  end
-
-  def new
+    @categories = Category.all
   end
 
   def show
+    @category = Category.find(params[:id])
   end
 
+  # admin only
+  def create
+    @category = Category.new(category_params)
+
+  	if @category.save
+  		redirect_to categories_path, notice: 'Category created'
+  	else
+  		render :new, notice: 'Something went wrong - please try again'
+  	end
+  end
+
+  # admin only
+  def new
+    @category = Category.new
+  end
+
+  # admin only
   def edit
+    @category = Category.find(params[:id])
   end
 
+  # admin only
   def update
+    @category = Category.find(params[:id])
+
+    if @category.update(category_params)
+      redirect_to categories_url
+    else
+      render "edit"
+    end
+
   end
 
+  # admin only
   def destroy
+    Category.find(params[:id]).delete
+    redirect_to categories_url
   end
+
+  private
+
+  def category_params
+  	params.require(:category).permit(:name)
+  end
+
 end
