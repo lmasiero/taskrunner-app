ddc11ea597fb4d93a59c1456ba7a93b2d238b3a4
Nick Ang <nickangtc@gmail.com>
2016-10-10 22:24:16 +0800

    Add services list to pro#show view

--- app/controllers/professionals_controller.rb
+++ app/controllers/professionals_controller.rb
@@ -1,6 +1,6 @@
 class ProfessionalsController < ApplicationController
   layout "professional", except: [:new]
-  before_action :is_authenticated_pro, except: [:new, :create]
+  before_action :is_authenticated_pro, except: [:new, :create, :show]
   before_action :set_professional, only: [:show, :edit, :update, :destroy]
 
   # GET /professionals
@@ -16,6 +16,7 @@ class ProfessionalsController < ApplicationController
     if current_user
       @current_user = current_user
       @requests = @professional.requests
+      @services = @professional.services
       puts @requests.inspect
     elsif current_professional
       @current_professional = current_professional
