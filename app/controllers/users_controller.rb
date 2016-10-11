d459f3a98109476ba61b86d0708ad7415d38a3d3
Nick Ang <nickangtc@gmail.com>
2016-10-11 09:42:04 +0800

    Add dynamic categories list for user#show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @professionals = Professional.all
+    @categories = Category.all
   end
 
   # GET /users/new
