17d0c5f541ece014571d81c7afdeabf9c1fe6731
Mikella Seth <mikellaseth@Mikellas-MacBook-Pro.local>
2016-10-12 17:49:16 +0800

    Fixed bug from table plugin

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
-    $('#table_search').DataTable();
-    $('#table_search_filter').css("margin-bottom","10px")
-    $('#table_search_previous').css("margin-bottom","10px")
+  var elementExists = document.getElementById("table_search");
+    if(elementExists){
+      $('#table_search').DataTable();
+      $('#table_search_filter').css("margin-bottom","10px")
+      $('#table_search_previous').css("margin-bottom","10px")
+    }
 } )
